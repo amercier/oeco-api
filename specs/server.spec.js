1bd39eb53863d9e9a6231432aa6499bb8d5b3feb
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-06-02 11:49:54 -0700

    Removed debugging statement

--- specs/server.spec.js
+++ specs/server.spec.js
@@ -16,7 +16,6 @@ describe('/', () => {
   describe('GET /', () => {
     it('returns 200 OK', () => supertest(server).get('/').expect(200));
     it('returns "Hello World!"', () => supertest(server).get('/').then(res => {
-      console.log(`body: ${res.body}`);
       expect(res.body).to.deep.equal('Hello World!');
     }));
   });
