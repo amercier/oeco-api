56f535ebd774153aad8be19695614b25d877ba5f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-06-02 21:13:18 -0700

    Added Mongoose

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const config = {
   port: process.env.PORT || 8080,
+  mongodb: {
+    host: 'localhost',
+    db: 'oeco-dev',
+  },
 };
 
 module.exports = function serverFactory() {
   const app = express();
-  const { port } = config;
+
+  const { host, db } = config.mongodb;
+  const mongoUrl = `mongodb://${host}/${db}`;
+  mongoose.connect(mongoUrl);
+  console.log(`Connected to MongoDB at ${mongoUrl}`);
 
   app.get('/', (request, response) => {
     response.send(`Hello World!\n`);
   });
 
+  const { port } = config;
   const server = app.listen(port, () => {
     console.log(`Application listening on port ${server.address().port}...`);
   });
