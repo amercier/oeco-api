6a9146a6a958928d59d2c8932ef5def4cb526f54
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-06-04 15:15:01 -0700

    Added POST /categories

--- app/schemas.js
+++ app/schemas.js
@@ -1,9 +1,12 @@
 const { Schema } = require('mongoose');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const { Slug } = require('./types');
 
 const Category = new Schema({
-  id: { type: Slug, trim: true, unique: true, index: true },
-  name: { type: String, trim: true, unique: true },
+  id: { type: Slug, required: true, trim: true, unique: true, index: true },
+  name: { type: String, required: true, trim: true, unique: true },
 });
+
+Category.plugin(beautifyUnique);
 
 module.exports = { Category };
--- app/server.js
+++ app/server.js
@@ -1,25 +1,54 @@
 const { promisify } = require('bluebird');
+const bodyParser = require('body-parser');
 const express = require('express');
 const { info } = require('./console');
 const config = require('./config');
 const setupMongoose = require('./db');
+
+function errorFormatter(response) {
+  return err => {
+    let code = err.code || 500;
+    let message = err.message || `${err}`;
+    if (err.name && err.name === 'ValidationError') {
+      code = 400;
+      message = err.errors[Object.keys(err.errors)[0]].message;
+    }
+    response.status(code).send({ message });
+  };
+}
 
 // Express
 module.exports = function serve() {
   return setupMongoose().then(({ db, models }) => new Promise(resolve => {
     const app = express();
 
+    app.use(bodyParser.json());
+
     app.get('/', (request, response) => {
       response.send(`Hello World!\n`);
     });
 
-    app.get('/categories', (request, response) => {
-      const query = models.Category.find({}).sort({ name: 1 });
-      promisify(query.exec.bind(query))().then(results => {
-        const categories = results.map(result => ({ id: result.id, name: result.name }));
-        response.json(categories);
+    const { Category } = models;
+
+    app.route('/categories')
+      .get((request, response) => {
+        Promise.resolve()
+          .then(() => {
+            const query = Category.find({}).sort({ name: 1 });
+            return promisify(query.exec.bind(query))();
+          })
+          .then(results => {
+            const categories = results.map(result => ({ id: result.id, name: result.name }));
+            response.json(categories);
+          })
+          .catch(errorFormatter(response));
+      })
+      .post((request, response) => {
+        Promise.resolve(request.body)
+          .then(payload => new Category({ id: payload.id, name: payload.name }).save())
+          .then(result => response.json({ id: result.id, name: result.name }))
+          .catch(errorFormatter(response));
       });
-    });
 
     const { port } = config;
     const server = app.listen(port, () => {
